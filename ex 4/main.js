586e24c89fa163ce50236159b75a244bfc8bc13b
HugoFranceschi <franceschi.hugo@yahoo.com>
2024-10-16 10:08:00 +0200

    feat(ex 4 and 5) EXo_JS2

---
+++ ex 4/main.js
@@ -0,0 +1,8 @@
+let age = 18;
+let nationalite = "française";
+
+if (age <= 18 && nationalite == "française") {
+	console.log(age, nationalite);
+} else {
+	console.log("pas majeur");
+}
---
+++ ex 5/main.js
@@ -0,0 +1,9 @@
+let age = 15;
+
+if (age <= 5) {
+	console.log("Gratuit");
+} else if (age <= 18) {
+	console.log("Réduit");
+} else {
+	console.log("Plein tarif");
+}
